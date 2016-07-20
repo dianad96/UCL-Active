9d7536d5712edeffdb0514dbf2cdfa1675f9c102
DianaD <dianadarie@yahoo.com>
2016-07-20 19:40:38 +0100

    tableview bug fixed

--- uclactive/ProfileController.swift
+++ uclactive/ProfileController.swift
@@ -13,7 +13,7 @@ let offset_HeaderStop:CGFloat = 40.0 // At this offset the Header stops its tran
 let distance_W_LabelHeader:CGFloat = 30.0 // The distance between the top of the screen and the top of the White Label
 
 enum contentTypes {
-    case Tweets, Media
+    case Performance, Details
 }
 
 class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate  {
@@ -31,13 +31,15 @@ class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataS
     //Class properties
     var headerBlurImageView:UIImageView!
     var headerImageView:UIImageView!
-    var contentToDisplay : contentTypes = .Tweets
+    var contentToDisplay : contentTypes = .Performance
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.contentInset = UIEdgeInsetsMake(headerView.frame.height, 0, 0, 0)
+        tableView.delegate=self
+        tableView.dataSource=self
     }
     
     
@@ -52,13 +54,13 @@ class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataS
         
         // Header - Blurred Image
         headerBlurImageView = UIImageView(frame: headerView.bounds)
-    
-        //!!!!!!!!
+        
         headerBlurImageView?.contentMode = UIViewContentMode.ScaleAspectFill
         headerBlurImageView?.alpha = 0.0
         headerView.insertSubview(headerBlurImageView, belowSubview: headerLabel)
         
         headerView.clipsToBounds = true
+
 
     }
     
@@ -77,11 +79,11 @@ class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataS
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         switch contentToDisplay {
-        case .Tweets:
-            return 40
+        case .Performance:
+            return 3
             
-        case .Media:
-            return 20
+        case .Details:
+            return 3
         }
         
     }
@@ -91,106 +93,24 @@ class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataS
         
         
         switch contentToDisplay {
-        case .Tweets:
-            cell.textLabel?.text = "Tweet Tweet!"
+        case .Performance:
+            cell.textLabel?.text = "Daily Steps"
             
-        case .Media:
-            cell.textLabel?.text = "Piccies!"
-            cell.imageView?.image = UIImage(named: "header_bg")
+        case .Details:
+            cell.textLabel?.text = "Height"
         }
 
         return cell
     }
     
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        let offset = scrollView.contentOffset.y + headerView.bounds.height
         
-        var avatarTransform = CATransform3DIdentity
-        var headerTransform = CATransform3DIdentity
-        
-        // PULL DOWN -----------------
-        
-        if offset < 0 {
-            
-            let headerScaleFactor:CGFloat = -(offset) / headerView.bounds.height
-            let headerSizevariation = ((headerView.bounds.height * (1.0 + headerScaleFactor)) - headerView.bounds.height)/2
-            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
-            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
-            
-            
-            // Hide views if scrolled super fast
-            headerView.layer.zPosition = 0
-            headerLabel.hidden = true
-            
-        }
-        // SCROLL UP/DOWN ------------
-            
-        else {
-            
-            // Header -----------
-            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
-            
-            
-            //  ------------ Label
-            headerLabel.hidden = false
-            let alignToNameLabel = -offset + handleLabel.frame.origin.y + headerView.frame.height + offset_HeaderStop
-            
-            headerLabel.frame.origin = CGPointMake(headerLabel.frame.origin.x, max(alignToNameLabel, distance_W_LabelHeader + offset_HeaderStop))
-            
-            //  ------------ Blur
-            headerBlurImageView?.alpha = min (1.0, (offset - alignToNameLabel)/distance_W_LabelHeader)
-            
-            // Avatar -----------
-            let avatarScaleFactor = (min(offset_HeaderStop, offset)) / avatarImage.bounds.height / 1.4 // Slow down the animation
-            let avatarSizeVariation = ((avatarImage.bounds.height * (1.0 + avatarScaleFactor)) - avatarImage.bounds.height) / 2.0
-            avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
-            avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
-            
-            if offset <= offset_HeaderStop {
-                
-                if avatarImage.layer.zPosition < headerView.layer.zPosition{
-                    headerView.layer.zPosition = 0
-                }
-                
-                
-            }else {
-                if avatarImage.layer.zPosition >= headerView.layer.zPosition{
-                    headerView.layer.zPosition = 2
-                }
-                
-            }
-            
-        }
-        
-        // Apply Transformations
-        headerView.layer.transform = headerTransform
-        avatarImage.layer.transform = avatarTransform
-        
-        // Segment control
-        
-        let segmentViewOffset = profileView.frame.height - segmentedView.frame.height - offset
-        
-        var segmentTransform = CATransform3DIdentity
-        
-        // Scroll the segment view until its offset reaches the same offset at which the header stopped shrinking
-        segmentTransform = CATransform3DTranslate(segmentTransform, 0, max(segmentViewOffset, -offset_HeaderStop), 0)
-        
-        segmentedView.layer.transform = segmentTransform
-        
-        
-        // Set scroll view insets just underneath the segment control
-        tableView.scrollIndicatorInsets = UIEdgeInsetsMake(segmentedView.frame.maxY, 0, 0, 0)
-        
-    }
-    
     
     @IBAction func selectContentType(sender: AnyObject) {
-        // crap code I know
         if sender.selectedSegmentIndex == 0 {
-            contentToDisplay = .Tweets
+            contentToDisplay = .Performance
         }
         else {
-            contentToDisplay = .Media
+            contentToDisplay = .Details
         }
         
         tableView.reloadData()
